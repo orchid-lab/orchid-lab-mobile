b0e2298a46b9be34a6d537a43a9f81b9c0619461
Phát Tài Tăng Nguyễn <92193419+tykyfatkie@users.noreply.github.com>
2026-04-15 19:03:38 +0700

    Update AppNavigator.tsx

--- __tests__/src/navigation/AppNavigator.tsx
+++ __tests__/src/navigation/AppNavigator.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthNavigator } from './AuthNavigator';
-// Import MainNavigator (dành cho Admin/Researcher) vào đây sau này
 
 export const AppNavigator = () => {
-  // Tạm thời hardcode biến này. Sau này sẽ dùng Zustand để lấy trạng thái thật
+  // Tạm thời hardcode biến này. Sau này sẽ dùng Zustand kết hợp AsyncStorage
   const isAuthenticated = false; 
 
   return (
     <NavigationContainer>
-      {isAuthenticated ? (
-        {/* <MainNavigator /> */}
-        <></> 
-      ) : (
-        <AuthNavigator />
-      )}
+      {/* Sau này tạo xong MainNavigator thì thay thế vào chỗ <></> */}
+      {isAuthenticated ? <></> : <AuthNavigator />}
     </NavigationContainer>
   );
 };
