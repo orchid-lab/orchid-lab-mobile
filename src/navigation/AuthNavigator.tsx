2e3e3da77287ce1686e74681db5e350da99e86cd
Phát Tài Tăng Nguyễn <92193419+tykyfatkie@users.noreply.github.com>
2026-04-16 11:01:41 +0700

    Revert "create quick menu"
    This reverts commit b8e26863ff2d6057f1d0d364e6843f0411dfc5b4.

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen/LoginScreen';
 import ReportsScreen from '../screens/Reports/ReportsScreen';
@@ -8,27 +6,23 @@ import ExperimentLogScreen from '../screens/ExperimentLog/ExperimentLogScreen';
 import SamplesScreen from '../screens/Samples/SamplesScreen';
 import BatchesScreen from '../screens/Batches/BatchesScreen';
 import TasksScreen from '../screens/Tasks/TasksScreen';
-import { QuickMenu } from '../components/QuickMenu';
 
 const Stack = createNativeStackNavigator();
 
 export const AuthNavigator = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <Stack.Navigator 
-        screenOptions={{ 
-          headerShown: false,
-          animation: 'none', 
-        }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="TechnicianReports" component={ReportsScreen} />
-        <Stack.Screen name="ExperimentLog" component={ExperimentLogScreen} />
-        <Stack.Screen name="Samples" component={SamplesScreen} />
-        <Stack.Screen name="Batches" component={BatchesScreen} />
-        <Stack.Screen name="Tasks" component={TasksScreen} />
-      </Stack.Navigator>
-      <QuickMenu />
-    </View>
+    <Stack.Navigator 
+      screenOptions={{ 
+        headerShown: false,
+        animation: 'none', 
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="TechnicianReports" component={ReportsScreen} />
+      <Stack.Screen name="ExperimentLog" component={ExperimentLogScreen} />
+      <Stack.Screen name="Samples" component={SamplesScreen} />
+      <Stack.Screen name="Batches" component={BatchesScreen} />
+      <Stack.Screen name="Tasks" component={TasksScreen} />
+    </Stack.Navigator>
   );
 };
